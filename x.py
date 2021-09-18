f8fda49371f888a1f99b7f1fd5ed435432b325a4
xushengfeng <xushengfeng_zg@163.com>
2021-09-18 21:30:34 +0800

    add orc

--- x.py
+++ x.py
@@ -1,11 +1,10 @@
-import keyboard
 import os
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap, QImage,QIcon
 from PyQt5.QtCore import Qt, pyqtSignal
 import pyautogui
 import pyperclip
-
+from paddleocr import PaddleOCR, draw_ocr
 
 # 划词
 def wording():
@@ -17,4 +16,12 @@ def wording():
     else:
         print(text)
 
-wording()
+# wording()
+
+def orc():
+    ocr = PaddleOCR(use_gpu=False,lang="ch") # 首次执行会自动下载模型文件
+    img_path = "aipeom.png"
+    result = ocr.ocr(img_path)
+    boxes = [line[0] for line in result]
+    txts = [line[1][0] for line in result]
+    scores = [line[1][1] for line in result]
